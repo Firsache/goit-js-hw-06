bd67f7d8cfc04912030697e7a8cbb4d5cf3f913e
Firsova Liubov <firsova.lyub@gmail.com>
2022-12-24 19:47:19 +0200

    Update task-05.js

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,3 +2,10 @@
 // input#name - input(подія input), підставляє його поточне значення
 // в span#name - output.Якщо інпут порожній, у спані повинен відображатися
 // рядок "Anonymous".
+
+const inputNameEl = document.querySelector('#name-input');
+const outputNameEl = document.querySelector('#name-output');
+
+inputNameEl.addEventListener('focus', evt => {
+    inputNameEl.style.outlineColor = 'red';
+});
