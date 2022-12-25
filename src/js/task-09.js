490904bacce0806e0e60182ef524d1bac5566804
Firsova Liubov <firsova.lyub@gmail.com>
2022-12-25 02:15:18 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -7,3 +7,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+
+
+const changeBtnBackgroundColor = evt => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+};
+buttonEl.addEventListener('click', changeBtnBackgroundColor);
